fa4975f262f5edb683db843c4a7b22306919951a
cody.morgan <cody.morgan@digipen.edu>
2019-10-17 16:28:18 -0700

    changing CS -> CPP

---
+++ ProjectLeetEuler/ProjectEulerCPP.cpp
@@ -0,0 +1,14 @@
+/******************************************************************************
+  * Project Euler Solutions
+  * Author : Cody Morgan
+  * Brief  : This is a collection of my solutions to ProjectEuler
+  *          and Leetcode problems mostly done for fun
+  *          This was orginally done in C# 2013 while I was deployed with the 
+  *          Army but I was a young and reckless programmer so now I'm doing 
+  *          it the right way - or at least a better way
+
+******************************************************************************/
+
+
+
+
